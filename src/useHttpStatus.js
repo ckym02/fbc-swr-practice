d9cecf14bccb40aeca36ae1f6a9770b6fbfd8bb5
koyama-c <koyama@linkedge.jp>
2023-11-30 01:09:19 +0900

    コール先のurlを元のものから変更しない

--- src/useHttpStatus.js
+++ src/useHttpStatus.js
@@ -1,9 +1,10 @@
 import useSWR from "swr";
 
 export const useHttpStatus = () => {
+  const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
   const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
-  const { data, error, isLoading } = useSWR("https://httpstat.us/200", fetcher);
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
   return {
     httpStatus: data,
