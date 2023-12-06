182f61b33ee1cd23feb532cf12e6bc1cd7059a4f
koyama-c <koyama@linkedge.jp>
2023-12-06 15:03:02 +0900

    ローディング中でもエラーの場合はエラーメッセージを表示するようにする

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { useHttpStatus } from "./useHttpStatus";
 function App() {
   const { httpStatus, isLoading, isError } = useHttpStatus();
 
-  if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Failed to load.</p>;
+  if (isLoading) return <p>Loading...</p>;
   return <p>Status:{httpStatus.description}</p>;
 }
 
